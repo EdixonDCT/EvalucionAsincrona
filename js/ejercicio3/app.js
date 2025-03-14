70471ea5e738ddfc00900d2047f10572e8dc2ed1
EdixonDCT <edixondelta2022@gmail.com>
2025-03-14 07:29:45 -0500

    ejercicio 3 sin comentar

---
+++ js/ejercicio3/app.js
@@ -0,0 +1,21 @@
+import { getUsuarios, getPost, getCommets} from "../modulos/index.js";
+const URL = "https://jsonplaceholder.typicode.com";
+
+const encontrarUsuario = async (postName) => {
+  const usuarios = await getUsuarios(URL);
+  return await Promise.all(
+    usuarios.map(async (usuario) => {
+        const posts = await getPost(URL, usuario);
+        await Promise.all(
+          posts.map(async (post) => {
+            if (postName.toLowerCase() == (post.title).toLowerCase()) {
+              const coments = await getCommets(URL, post);
+              console.log({ ...post, coments });
+            }
+          })
+        );
+    })
+  );
+};
+let postName = prompt("Ingrese el nombre del post que desea buscar");
+encontrarUsuario(postName).then();
